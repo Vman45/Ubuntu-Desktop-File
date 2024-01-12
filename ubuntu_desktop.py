2bedea0a9ff45ea39c698a16440f329d7e794ae0
Bruno <el_diablo76@msn.com>
2024-01-12 05:31:09 +0100

    modified:   ubuntu_desktop.py

--- ubuntu_desktop.py
+++ ubuntu_desktop.py
@@ -17,17 +17,18 @@ class CollectDatas(UiUbuntuDesktop):
         super().__init__()
         self.dict_datas = {}
         self.title = "UDesktopFile"
-        # Connect pushButton
+        # Connect pushButtons
         self.pushButton_exec.clicked.connect(self.get_exec)
         self.pushButton_icon.clicked.connect(self.get_icon)
         self.pushButton_save.clicked.connect(self.get_all_datas)
         self.pushButton_quit.clicked.connect(app.exit)
         self.pushButton_categories.clicked.connect(self.get_categories)
+        # Connect checkBoxes
         self.checkBox_terminal.clicked.connect(self.set_statut_checkbox)
         self.checkBox_startup.clicked.connect(self.set_statut_checkbox)
         self.checkBox_directory.clicked.connect(self.set_path_directory)
 
-    def get_all_datas(self):
+    def get_all_datas(self) -> None:
         self.dict_datas = {
             "Categories": self.lineEdit_categories.text(),
             "Comment": self.lineEdit_comment.text(),
@@ -45,38 +46,38 @@ class CollectDatas(UiUbuntuDesktop):
         }
         self.save_desktop_file()
 
-    def set_statut_checkbox(self):
+    def set_statut_checkbox(self) -> None:
         self.sender().setText(str(self.sender().isChecked()))
     
-    def set_path_directory(self):
+    def set_path_directory(self) -> None:
         if self.checkBox_directory.isChecked():
             self.checkBox_directory.setText(os.path.dirname(self.lineEdit_exec.text()))
         else:
             self.checkBox_directory.setText("")
 
-    def open_dialog(self, title):
+    def open_dialog(self, title) -> str:
         dialog = DialogOpen("", title, "", "")
         return dialog.openFile()
 
-    def save_dialog(self, title):
+    def save_dialog(self, title) -> str:
         dialog = DialogOpen(
             "", title, f"{self.lineEdit_name.text()}.desktop", "*.desktop"
         )
         return dialog.saveFile()
 
-    def get_exec(self):
+    def get_exec(self) -> None:
         if binary_name := self.open_dialog("Select binary file"):
             self.lineEdit_exec.setText(binary_name)
             self.set_path_directory()
 
-    def get_icon(self):
+    def get_icon(self) -> None:
         if icon_name := self.open_dialog("Select icon file"):
             self.lineEdit_icon.setText(icon_name)
 
-    def get_categories(self):
+    def get_categories(self) -> None:
         UiCategories(self)
 
-    def write_desktop_file(self, destination):
+    def write_desktop_file(self, destination) -> bool:
         try:
             datas_file = "[Desktop Entry]\n" + "\n".join(
                 f"{key}={value}" for key, value in self.dict_datas.items()
@@ -88,7 +89,7 @@ class CollectDatas(UiUbuntuDesktop):
             self.show_message(self.title, f"Unable to create file !! {er}")
             return False
 
-    def save_desktop_file(self):
+    def save_desktop_file(self) -> None:
         if self.lineEdit_name.text():
             if destination := self.save_dialog("Destination Desktop File"):
                 if self.write_desktop_file(destination):
@@ -96,7 +97,7 @@ class CollectDatas(UiUbuntuDesktop):
         else:
             self.show_message(self.title, "Please enter a File Name.")
 
-    def show_message(self, title, message):
+    def show_message(self, title, message) -> None:
         QtWidgets.QMessageBox.information(self, title, message)
 
 
