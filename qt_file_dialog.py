d4a3dac1eb71e87ac9eaf13a5b1424a8d6d59a0a
Diablo <el_diablo76@msn.com>
2024-01-15 16:24:45 +0100

    modifié :         qt_file_dialog.py

--- qt_file_dialog.py
+++ qt_file_dialog.py
@@ -3,9 +3,9 @@
 # le 05/07/2018 Copyright Diablo76
 # Sélecteur de fichiers et répertoires V1.0.0
 
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
-class FileDialog(QtWidgets.QFileDialog):
+class FileDialog(QFileDialog):
 
     def __init__(self, fileName, titre, repertoire, filtres):
         super().__init__()
@@ -15,9 +15,8 @@ class FileDialog(QtWidgets.QFileDialog):
         self.filtres = filtres
 
     def open_file(self):
-        file_name = self.getOpenFileName(None, self.titre, self.repertoire,
-                    self.filtres, options=self.DontUseNativeDialog)
-        return(file_name[0])
+        return self.getOpenFileName(None, self.titre, self.repertoire,
+                    self.filtres, options=self.DontUseNativeDialog)[0]
 
     def open_dir(self):
         return self.getExistingDirectory(
@@ -25,6 +24,5 @@ class FileDialog(QtWidgets.QFileDialog):
         )
 
     def save_file(self):
-        file_save = self.getSaveFileName(None, self.titre, self.repertoire,  
-                    self.filtres, options=self.DontUseNativeDialog)
-        return file_save[0]
+        return self.getSaveFileName(None, self.titre, self.repertoire,  
+                    self.filtres, options=self.DontUseNativeDialog)[0]
